709df7a3a4b59cf2a774d609890a722f100b6efa
Samiul Azam <samiulazam16@gmail.com>
2026-04-25 20:18:52 +0600

    checkpoint before checking out main

---
+++ public_html/application/logs/log-2026-04-25.php
@@ -0,0 +1,3 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2026-04-25 18:08:43 --> Severity: error --> Exception: Missing required database environment variables for production: CI_DB_HOST, CI_DB_USER, CI_DB_PASSWORD, CI_DB_NAME C:\Users\Samiul\Pictures\doctor\public_html\application\config\database.php 80
